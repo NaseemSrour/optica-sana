7a7780bddc1ed608cbfaaf629f39d11912d40d34
Naseem Srour <naseemsr94@gmail.com>
2025-12-29 13:06:57 +0200

    Updated Samer Notes in sql_queries.py

--- db/sql_queries.py
+++ db/sql_queries.py
@@ -1,12 +1,11 @@
 ADD_GLASSES_TEST_QUERY = """            INSERT INTO glasses_tests (
                 customer_id, exam_date, examiner,
-                r_fv_numerator, r_fv_denominator,
-                r_sphere, r_cylinder, r_axis, r_prism, r_base, r_va, r_add_read, r_add_int, r_add_bif, r_add_mul, r_high,
-                l_fv_numerator, l_fv_denominator, l_sphere, l_cylinder, l_axis, l_prism, l_base, l_va, l_add_read, l_add_int, l_add_bif, l_add_mul, l_high,
-                pupil_distance, dominant_eye, iop, glasses_role, lenses_material, lenses_diameter, segment_diameter, lenses_manufacturer, lenses_color, catalog_num, frame_manufacturer, frame_supplier, frame_model, frame_size, frame_bar_length, frame_color,
+                r_fv, r_sphere, r_cylinder, r_axis, r_prism, r_base, r_va, both_va, r_add_read, r_add_int, r_add_bif, r_add_mul, r_high, r_pd, sum_pd, near_pd,
+                l_fv, l_sphere, l_cylinder, l_axis, l_prism, l_base, l_va, l_add_read, l_add_int, l_add_bif, l_add_mul, l_high, l_pd,
+                dominant_eye, r_iop, l_iop, glasses_role, lenses_material, lenses_diameter_1, lenses_diameter_2, lenses_diameter_decentration_horizontal, lenses_diameter_decentration_vertical, segment_diameter, lenses_manufacturer, lenses_color, lenses_coated, catalog_num, frame_manufacturer, frame_supplier, frame_model, frame_size, frame_bar_length, frame_color,
                 diagnosis, notes
             )
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 
 orig_ADD_GLASSES_TEST_QUERY = """ 
             INSERT INTO glasses_tests (
@@ -21,10 +20,9 @@ orig_ADD_GLASSES_TEST_QUERY = """
 UPDATE_GLASSES_TEST_QUERY = """
             UPDATE glasses_tests
             SET customer_id=?, exam_date=?, examiner=?,
-                r_fv_numerator=?, r_fv_denominator=?,
-                r_sphere=?, r_cylinder=?, r_axis=?, r_prism=?, r_base=?, r_va=?, r_add_read=?, r_add_int=?, r_add_bif=?, r_add_mul=?, r_high=?,
-                l_fv_numerator=?, l_fv_denominator=?, l_sphere=?, l_cylinder=?, l_axis=?, l_prism=?, l_base=?, l_va=?, l_add_read=?, l_add_int=?, l_add_bif=?, l_add_mul=?, l_high=?,
-                pupil_distance=?, dominant_eye=?, iop=?, glasses_role=?, lenses_material=?, lenses_diameter=?, segment_diameter=?, lenses_manufacturer=?, lenses_color=?, catalog_num=?, frame_manufacturer=?, frame_supplier=?, frame_model=?, frame_size=?, frame_bar_length=?, frame_color=?,
+                r_fv=?, r_sphere=?, r_cylinder=?, r_axis=?, r_prism=?, r_base=?, r_va=?, both_va=?, r_add_read=?, r_add_int=?, r_add_bif=?, r_add_mul=?, r_high=?, r_pd=?, sum_pd=?, near_pd=?,
+                l_fv=?, l_sphere=?, l_cylinder=?, l_axis=?, l_prism=?, l_base=?, l_va=?, l_add_read=?, l_add_int=?, l_add_bif=?, l_add_mul=?, l_high=?, l_pd=?,
+                dominant_eye=?, r_iop=?, l_iop=?, glasses_role=?, lenses_material=?, lenses_diameter_1=?, lenses_diameter_2=?, lenses_diameter_decentration_horizontal=?, lenses_diameter_decentration_vertical=?, segment_diameter=?, lenses_manufacturer=?, lenses_color=?, lenses_coated=?, catalog_num=?, frame_manufacturer=?, frame_supplier=?, frame_model=?, frame_size=?, frame_bar_length=?, frame_color=?,
                 diagnosis=?, notes=?
             WHERE id=?
         """
